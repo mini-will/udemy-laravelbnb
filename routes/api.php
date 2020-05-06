c1a6dee8858c99a7b3ee6d81e082e89664d3ab80
mini-will <apple0minimino@gmail.com>
2020-05-06 18:26:16 +0900

    [Update] 32. Routes and model all() method

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Bookable;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('bookables', function (Request $request) {
+    return Bookable::all();
 });
