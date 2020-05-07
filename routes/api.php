dcc88b12fc5d30975d22569ef2c8dfde4e4a9ac1
mini-will <apple0minimino@gmail.com>
2020-05-08 07:25:19 +0900

    [Update] 44. Resource controllers

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 // Route::get('bookables', function (Request $request) {
 //     return Bookable::all();
@@ -25,5 +25,7 @@ use Illuminate\Support\Facades\Route;
 //     return Bookable::findOrFail($id);
 // });
 
-Route::get('bookables', 'Api\BookableController@index');
-Route::get('bookables/{id}', 'Api\BookableController@show');
+// Route::get('bookables', 'Api\BookableController@index');
+// Route::get('bookables/{id}', 'Api\BookableController@show');
+
+Route::apiResource('bookables', 'Api\BookableController')->only('index', 'show');
