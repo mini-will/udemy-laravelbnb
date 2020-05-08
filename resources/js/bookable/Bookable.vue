f2c8d303a7aee9b6af60166d45809305945f73b6
mini-will <apple0minimino@gmail.com>
2020-05-09 08:39:56 +0900

    [Update] 46. Availability component (horizontal forms, scoped styles)

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="row">
-    <div class="col-md-8">
-      <div class="card-body">
-        <div v-if="!loading">
-          <h2>{{ bookable.title }}</h2>
-          <hr />
-          <article>{{ bookable.description }}</article>
+    <div class="col-md-8 pb-4">
+      <div class="card">
+        <div class="card-body">
+          <div v-if="!loading">
+            <h2>{{ bookable.title }}</h2>
+            <hr />
+            <article>{{ bookable.description }}</article>
+          </div>
+          <div v-else>loading....</div>
         </div>
-        <div v-else>loading....</div>
       </div>
     </div>
-    <div class="col-md-4">availability & process</div>
+    <div class="col-md-4 pb-4">
+      <availability></availability>
+    </div>
   </div>
 </template>
 
 
 <script>
+import Availability from "./Availability";
+
 export default {
+  components: {
+    Availability
+  },
   data() {
     return {
       bookable: null,
@@ -26,7 +35,7 @@ export default {
   created() {
     this.loading = true;
     axios.get(`/api/bookables/${this.$route.params.id}`).then(response => {
-      this.bookable = response.data;
+      this.bookable = response.data.data;
       this.loading = false;
     });
   }
