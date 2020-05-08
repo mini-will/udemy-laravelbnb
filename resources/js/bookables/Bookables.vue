b6fe54564120686cb3965e249d17b8acf2846273
mini-will <apple0minimino@gmail.com>
2020-05-09 08:05:38 +0900

    [Update] 45. API Resources

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -51,7 +51,7 @@ export default {
     this.loading = true;
 
     const request = axios.get("/api/bookables").then(response => {
-      this.bookables = response.data;
+      this.bookables = response.data.data; //JsonResourceでdataが1つラッピングされるため
       this.loading = false;
     });
 
