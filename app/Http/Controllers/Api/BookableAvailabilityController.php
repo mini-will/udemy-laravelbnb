55afb24072ad16b560e832fd06abcf82f6463ae9
mini-will <apple0minimino@gmail.com>
2020-05-10 10:53:10 +0900

    [Update] 55. Returning bookable availability (HTTP response statuses)

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -24,6 +24,9 @@ class BookableAvailabilityController extends Controller
         );
 
         $bookable = Bookable::findOrFail($id);
-        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
+
+        return $bookable->availabelFor($data['from'], $data['to'])
+            ? response()->json([])
+            : response()->json([], 404);
     }
 }
