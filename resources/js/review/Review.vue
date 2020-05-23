9eaaacb84af597dfaeb680f68798b57319e46605
mini-will <apple0minimino@gmail.com>
2020-05-23 16:18:00 +0900

    [Update] 73. v-model explained, adding v-model support to custom components

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="form-group">
       <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-      <star-rating :value="review.rating" class="fa-3" v-on:rating:changed="review.rating = $event"></star-rating>
+      <star-rating class="fa-3x" v-model="review.rating"></star-rating>
     </div>
 
     <div class="form-group">
